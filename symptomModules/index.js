6f4b2888c62a9c3d76c54431a348934fc7d56e32
DTAB85 <adambrown.mba@gmail.com>
2025-08-28 13:38:20 -0500

    Update index.js

--- symptomModules/index.js
+++ symptomModules/index.js
@@ -1,1 +1,10 @@
+import chestPain from './chestPain';
+
+// This object maps internal symptom keys to their logic modules.
+// Add future symptom modules like: import cough from './cough' and include in the object.
+const symptomModules = {
+  'chest_pain': chestPain
+};
+
+export default symptomModules;
 
